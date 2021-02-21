2d07542fd653f49a838248561fc65bd535f9670d
jarray42 <jarray42@gmail.com>
2021-02-21 18:11:22 +0100

    Update atom_vec_body.cpp

--- src/atom_vec_body.cpp
+++ src/atom_vec_body.cpp
@@ -1,7 +1,7 @@
 /* ----------------------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    https://lammps.sandia.gov/, Sandia National Laboratories
-   Steve Plimpton, sjplimp@sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov 
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
